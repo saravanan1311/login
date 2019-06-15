213b6ecd9e1b7711578125a0a5eee9eaeabbfb2c
saravanan1311 <48938421+saravanan1311@users.noreply.github.com>
2019-06-15 21:25:36 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+	<title>Login Page</title>
+	<link rel="icon"  href=" ">
+	<link rel="stylesheet" type="text/css" href="login.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div id="header"></div>
+	<div id="nav"></div>
+	<div id="container">
+	<br /><br /><br /><br /><br /><br /> <br /><br /> <br /><br />
+	<div class="signin">
+	<form action="adminaction.php" method="post">
+		<table align="center" id="tab">
+			<tr>
+				<h2 style="color:#666;">Log In <i class="fa fa-sign-in" aria-hidden="true"></i> </h2><br /><br /><br />
+			</tr>
+			<tr>
+				<input type="text" name="username" placeholder="Username"/><br /><br /><br />
+			</tr>
+
+			<tr>
+				<input type="password" name="password" placeholder="Password" /><br /><br /><br />
+			</tr>
+			<tr>
+				<input type="submit" value="Log In"  name="login"  /> <br /><br />
+			</tr>
+
+		</table>
+	    </div><br /><br /><br /><br /><br /> 
+	    
+	</form>
+	</div>
+
+	<div id="header"></div>
+	<div id="footer"></div>
+</body>
+</html>
